2d9aa5182d20ca7d831b4c959e98796d8f1a8366
yasmin <yasminrhamouda@hotmail.com>
2024-03-23 19:03:55 +0000

    added an alert

--- js/main.js
+++ js/main.js
@@ -2,4 +2,7 @@ $(document).ready(function(event){
     // jQuery code 
     $("span").css("color", "red");
     $("#you").prop("disabled", true);
+    $("#submit").click(function(){
+        alert("You have submitted your final answer.");
+      });
   });
