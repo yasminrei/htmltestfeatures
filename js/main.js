4c55f753a7ae92ab011e6d161acd126511ebe38c
yasmin <yasminrhamouda@hotmail.com>
2024-03-23 20:39:17 +0000

    fieldset now fades out after hinge

--- js/main.js
+++ js/main.js
@@ -3,10 +3,13 @@ $(document).ready(function(event){
     $("#you").prop("disabled", true);
     $("#submit").click(function(){
         alert("You have submitted your final answer.");
-        $("fieldset").fadeOut("slow", function(event) {
-            $(this).remove();
-        });
+        // $("fieldset").fadeOut("slow", function(event) {
+        //     $(this).remove();
+        // });
+        $("fieldset").addClass("animate__hinge");
+        $("fieldset").delay(1500).fadeOut();
         $("footer").addClass("center-screen");
         $("span").css("color", "red");
       });
   });
+  
